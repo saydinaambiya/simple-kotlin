38771b169e8602645d1afaecc4736f463f2b48b2
saydinaambiya <ssyyddn@gmail.com>
2024-10-19 20:55:27 +0700

    indexing, escaped and raw string

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,51 +1,15 @@
 package org.example
 
 fun main() {
-    // indexing
-    val firstText = "android"
-    val fourthCharacter = firstText[3]
-    println("The 4th character of the $firstText is $fourthCharacter")
-
-    val secondText = "kotlin"
-    for (character in secondText) {
-        print("$character ")
-    }
-
-    // escaped
-    val statement = "Kotlin is \"Awesome!\""
-    val symbol = "Unicode test: \u00A9"
-    println(statement)
-    println(symbol)
-
-    // raw
-    val line = """
-        Line 1
-        Line 2
-        Line 3
-    """.trimIndent()
-    print(line)
+    val name = "kotlin"
+    val old = 3
+    val hour = 7
+    println("My name is $name, i am $old years old")
+    print("Office ${if (hour > 7) "already close" else "is open"}")
 }
 
 /*
     [output]
-    -- indexing --
-    The 4th character of the android is r
-    k o t l i n
-
-    -- escaped --
-    Kotlin is "Awesome!"
-    Unicode test: ©
-
-    -- raw --
-    Line 1
-    Line 2
-    Line 3
-
-    [explain]
-    -- escaped --
-    \t = tab
-    \n = new line
-    \' = single quote
-    \" = double quote
-    \\ = backslash
+    My name is kotlin, i am 3 years old
+    Office is open
  */
