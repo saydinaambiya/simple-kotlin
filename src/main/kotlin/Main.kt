f5c3449128ac2253d1adeb78990b5d5c440cc30d
saydinaambiya <ssyyddn@gmail.com>
2024-11-24 18:18:37 +0700

    use vararg and spread separator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,15 +2,24 @@ package org.example
 
 
 fun main() {
-    greetingUser("Kotlin")
+    val resultSum = sumNumbers(1, 2, 3)
+    print(resultSum)
+    val resultTechAndLang = setTechAndLang("backend", "java, kotlin")
+    print(resultTechAndLang)
+
+    // the difference array and vararg
+    val numbers = intArrayOf(1, 2, 3, 4, 5)
+    sumNumbers(*numbers) // can use spread separator
 }
 
-fun greetingUser(name: String) { // :Unit like void in Java
-    println("Hello $name")
+fun sumNumbers(vararg numbers: Int): Int { // vararg is array
+    return numbers.sum()
 }
 
-
+fun setTechAndLang(tech: String, vararg lang: String): String { // best practice is set the vararg at last parameters
+    return "You can learn $tech using ${lang.toList()}"
+}
 /*
     output:
-    Hello Kotlin
+    6
  */
