afab6bd22e90a951b86ca9d86639102faad170d8
saydinaambiya <ssyyddn@gmail.com>
2024-11-24 17:57:30 +0700

    try unit in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,16 +2,15 @@ package org.example
 
 
 fun main() {
-    // safe call operator
-    val text: String? = null
-    text?.length
-
-    // elvis operator
-    val nullText: String? = null
-    val textLength = nullText?.length ?: 7
-    val nextTextLength = nullText!!.length
+    greetingUser("Kotlin")
 }
+
+fun greetingUser(name: String) { // :Unit like void in Java
+    println("Hello $name")
+}
+
+
 /*
     output:
-
+    Hello Kotlin
  */
