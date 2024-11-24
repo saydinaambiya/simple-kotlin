ab9efe9e4aa4686fc7858c38faec90d38f774ab8
saydinaambiya <ssyyddn@gmail.com>
2024-11-24 17:06:45 +0700

    convert numbers and readable numbers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,22 @@
 package org.example
 
-import java.time.LocalDateTime
 
 fun main() {
-    val year = LocalDateTime.now().year
-    val month = LocalDateTime.now().monthValue
-    val today = LocalDateTime.now().dayOfMonth
-    val openHours = LocalDateTime.of(year, month, today, 7, 0).hour
-    val now = LocalDateTime.now().hour
-    val office: String = if (now>openHours){
-        "Office already open"
-    }else if(now == openHours){
-        "Wait a minute, office will be open"
-    }else{
-        "Office is closed"
-    }
-    print(office)
+    val byteNumber: Byte = 10
+    val intNumber: Int = byteNumber.toInt()
+    println(intNumber)
+
+    val stringNumber = "10"
+    val stringToIntNumber: Int = stringNumber.toInt()
+    println(stringToIntNumber)
+
+    val readableNumber = 1_000
+    println(readableNumber)
 }
 
 /*
     [output]
-    depend on the hour this code will execute
-
-    note: kotlin not support ternary operator cause replace by normal if expressions
+    10
+    10
+    1000
  */
